46ef684a0fe00de937d9e89421ff2ed1a219acdc
safamahmoud71 <fuckyoubtw999>
2022-12-23 21:06:44 +0200

    ieee

---
+++ android/app/src/main/kotlin/com/example/task3loginieee/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.task3loginieee
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/sigup.dart
@@ -0,0 +1,258 @@
+import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+import 'package:task3loginieee/login.dart';
+class SigUP extends StatefulWidget {
+  const SigUP({Key? key}) : super(key: key);
+
+  @override
+  State<SigUP> createState() => _SigUPState();
+}
+
+class _SigUPState extends State<SigUP> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+
+      body: SingleChildScrollView(
+        child: Center(
+          child: Column(
+            children: [
+              Padding(
+                padding: const EdgeInsets.only(top: 70.0, bottom: 20),
+                child: Center(
+                  child: Text(" Welcome to IEEE" , style:
+                  TextStyle(color: Colors.blue,
+                      fontWeight: FontWeight.bold,
+                      fontSize: 30),),
+                ),
+              ),
+              Padding(
+                padding: const EdgeInsets.all(8.0),
+                child: Text("Sign UP" , style:
+                TextStyle(color: Colors.blue,
+                    fontWeight: FontWeight.bold,
+                    fontSize: 30),),
+              ),
+              SizedBox(
+                height: 20,
+              ),
+              Padding(
+                padding: const EdgeInsets.all(20),
+                child: TextFormField(
+
+
+                  decoration:  InputDecoration(
+
+                      border: OutlineInputBorder(
+                        borderRadius: BorderRadius.all(
+                          Radius.circular(10),
+
+                        ),
+                      ),
+
+
+                      labelText: 'Name',
+                      labelStyle: GoogleFonts.poppins(
+                          color: Colors.grey[700]
+                      ),
+                      focusedBorder: OutlineInputBorder(
+                          borderSide: BorderSide(
+                              color: Colors.blue
+
+                          )
+                      )
+
+
+                  ),
+                ),
+              ),
+              Padding(
+                padding: const EdgeInsets.all(20),
+                child: TextFormField(
+
+
+                  decoration:  InputDecoration(
+                      suffixIcon: IconButton(
+                        onPressed: (){
+                          bool ispressed = true ;
+                          showPassword(){
+                            ispressed=! ispressed;}
+                        }, icon: Icon(Icons.email
+                      ),),
+                      border: OutlineInputBorder(
+                        borderRadius: BorderRadius.all(
+                          Radius.circular(10),
+
+                        ),
+                      ),
+
+
+                      labelText: 'E-mail',
+                      labelStyle: GoogleFonts.poppins(
+                          color: Colors.grey[700]
+                      ),
+                      focusedBorder: OutlineInputBorder(
+                          borderSide: BorderSide(
+                              color: Colors.blue
+
+                          )
+                      )
+
+
+                  ),
+                ),
+              ),
+              Padding(
+                padding: const EdgeInsets.all(20),
+                child: TextFormField(
+
+
+                  decoration:  InputDecoration(
+
+                      border: OutlineInputBorder(
+                        borderRadius: BorderRadius.all(
+                          Radius.circular(10),
+
+                        ),
+                      ),
+
+
+                      labelText: 'Phone Number',
+                      labelStyle: GoogleFonts.poppins(
+                          color: Colors.grey[700]
+                      ),
+                      focusedBorder: OutlineInputBorder(
+                          borderSide: BorderSide(
+                              color: Colors.blue
+
+                          )
+                      )
+
+
+                  ),
+                ),
+              ),
+              Padding(
+                padding: const EdgeInsets.all(20),
+                child: TextFormField(
+
+
+                  decoration:  InputDecoration(
+                      suffixIcon: IconButton(
+                        onPressed: (){
+
+                        }, icon: Icon(Icons.remove_red_eye_rounded
+                      ),),
+                      border: OutlineInputBorder(
+                        borderRadius: BorderRadius.all(
+                          Radius.circular(10),
+
+                        ),
+                      ),
+
+
+                      labelText: 'Password',
+                      labelStyle: GoogleFonts.poppins(
+                          color: Colors.grey[700]
+                      ),
+                      focusedBorder: OutlineInputBorder(
+                          borderSide: BorderSide(
+                              color: Colors.blue
+
+                          )
+                      )
+
+
+                  ),
+                ),
+              ),
+              Padding(
+                padding: const EdgeInsets.only(left: 20),
+                child: TextButton(onPressed: (){}, child:
+                Text("Forget Password?",
+                  style: TextStyle(
+                      color: Colors.blue,
+                      decoration: TextDecoration.underline
+                  ),)),
+              ),
+              Padding(
+                padding: const EdgeInsets.only(left: 20, right: 20, top: 40),
+                child: ElevatedButton(onPressed: (){
+
+
+
+
+                },
+                  child:
+                  Text('Sign Up',
+                    style: TextStyle(
+                        color: Colors.white,
+                        fontSize: 20 ,
+                        fontWeight: FontWeight.bold
+                    ),) , style: ElevatedButton.styleFrom(
+                      fixedSize: Size(370, 50),
+                      backgroundColor: Colors.blue,
+                      shape: RoundedRectangleBorder(
+                          borderRadius: BorderRadius.circular(10)
+                      )
+
+                  ),),
+
+              ),
+              Row(
+                children: [
+                  Expanded(
+                    child: Divider(
+                      color: Colors.grey, //color of divider
+                      height: 50, //height spacing of divider
+                      thickness: 1, //thickness of divier line
+                      indent: 20, //spacing at the start of divider
+                      endIndent: 10, //spacing at the end of divider
+                    ),
+                  ),
+                  Text('OR',style: TextStyle(
+                      color: Colors.black,
+                      fontSize: 20
+                  ),),
+                  Expanded(
+                    child: Divider(
+                      color: Colors.grey, //color of divider
+                      height: 20, //height spacing of divider
+                      thickness: 1, //thickness of divier line
+                      indent: 20, //spacing at the start of divider
+                      endIndent: 10, //spacing at the end of divider
+                    ),
+                  ),
+
+                ],
+              ),
+              Padding(
+                padding: const EdgeInsets.only(left: 20, right: 20, top: 20),
+                child: ElevatedButton(onPressed: (){
+                  Navigator.push(context, MaterialPageRoute(builder: (context)=> LoginScreen()));
+                }, child:
+                Text('Login',
+                  style: TextStyle(
+                      color: Colors.blue,
+                      fontSize: 20 ,
+                      fontWeight: FontWeight.bold
+                  ),) , style: ElevatedButton.styleFrom(
+                    fixedSize: Size(370, 50),
+                    backgroundColor: Colors.white,
+                    shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(10),
+                        side: BorderSide(color: Colors.blue,
+                            width: 2.5)
+                    )
+
+                ),),
+
+              ),
+
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
